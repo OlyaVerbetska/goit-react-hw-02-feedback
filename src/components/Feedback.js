6dbfad1ee36663f077491f9c4c2b928671f86ec6
Olga Verbetska <verbetska150@gmail.com>
2021-03-30 16:11:12 +0300

    step 1+2

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,27 +1,68 @@
 import React from 'react';
 
 class Feedback extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     good: 0,
+  //     neutral: 0,
+  //     bad: 0,
+  //   };
+  // }
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleGoodButton = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  handleNeutralButton = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleBadButton = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+  countTotalFeedback = () => {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    const average = Math.round((this.state.good / total) * 100);
+    if (isNaN(average)) {
+      return '100';
+    }
+    return average;
+  };
+
   render() {
     return (
       <div>
         <p>Please leave Feedback</p>
         <ul>
           <li>
-            <button type="button">Good</button>
+            <button type="button" onClick={this.handleGoodButton}>
+              Good
+            </button>
           </li>
           <li>
-            <button type="button">Neutral</button>
+            <button type="button" onClick={this.handleNeutralButton}>
+              Neutral
+            </button>
           </li>
           <li>
-            <button type="button">Bad</button>
+            <button type="button" onClick={this.handleBadButton}>
+              Bad
+            </button>
           </li>
         </ul>
         <p>Statistics</p>
@@ -34,6 +75,12 @@ class Feedback extends React.Component {
           </li>
           <li>
             <span>Bad:{this.state.bad}</span>
+          </li>
+          <li>
+            <span>Total:{this.countTotalFeedback()}</span>
+          </li>
+          <li>
+            <span>Positive feedback:{this.countPositiveFeedbackPercentage()}%</span>
           </li>
         </ul>
       </div>
